d4d098b7aa6783568e6db899ce9b7004a3cb3740
GovindSinghRajput680 <145134257+GovindSinghRajput680@users.noreply.github.com>
2023-09-19 02:09:50 +0530

    Add files via upload

---
+++ Q1245code.cpp.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){//Q 1
+cout<<"Physics "<<endl<<"Wallah";
+
+//Q 2
+int i = 5 +4;
+cout<<++i;
+
+//Q 4
+int a = 4;
+int b = 5;
+a++;b--;
+cout<<endl<<++a<<" "<<b--;
+
+//Q 5
+int r = 10;
+cout<<endl<<"circumference of the circle  = "<<3.14*r*r;
+
+
+}
